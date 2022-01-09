3da87a85dccb0427b0f91536673657b17c25f15f
Matias Rondinella <matiasrondinellaphoto@gmail.com>
2022-01-09 20:04:56 -0300

    agregue prompts y alerts al index.html

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,14 @@
-let nombre = "";
-let edad = 0;
-let pais = "";
+//let nombre = "";
+//let edad = 0;
+//let pais = "";
 
-let datoUsuario = nombre + edad + pais
+//let datoUsuario = nombre + edad + pais 
 
-console.log (datoUsuario)
+//console.log (datoUsuario)
 alert ("Bienvenidos a nuestra comunidad")
+alert("Te vamos a pedir unos datos")
+let nombreUsuario = prompt ("¿Cual es tu nombre?")
+alert ("Bienvenido/a " + nombreUsuario)
+prompt (nombreUsuario + "," + " ¿Quieres aprender a cuidar mejor de nuestro planeta?")
+alert ("Pues manos a la obra")
+console.log(nombreUsuario)
