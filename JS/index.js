9d70eaebfeea1c994ca6e29851829eb1d016b6b9
Matias <matiasrondinellaphoto@gmail.com>
2022-01-10 18:12:28 -0300

    lo logre

--- JS/index.js
+++ JS/index.js
@@ -6,6 +6,16 @@
 
 //console.log (datoUsuario)
 
+let nombreUsuario = prompt ("Bienvenido, ingrese su nombre")
+if (nombreUsuario > 0) {
+    alert("Por favor, ingrese un nombre valido")
+    
+} else if ((nombreUsuario < 10000) || (nombreUsuario == Number)) {
+    alert ("Por favor, ingrese un nombre valido")
+} else{
+    alert ("Bienvenido/a " + nombreUsuario)
+}
+
 
 
 
