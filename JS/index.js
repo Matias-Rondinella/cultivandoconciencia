a7c95af0940ab8aa91dbf9b57122bb0ccf402544
Matias <matiasrondinellaphoto@gmail.com>
2022-01-10 15:20:44 -0300

    Limpie el espacio de trabajo y clone el repositorio de forma exitosa a mi laptop

--- JS/index.js
+++ JS/index.js
@@ -7,10 +7,3 @@
 //console.log (datoUsuario)
 
 
-alert ("Bienvenidos a nuestra comunidad")
-alert("Te vamos a pedir unos datos")
-let nombreUsuario = prompt ("¿Cual es tu nombre?")
-alert ("Bienvenido/a " + nombreUsuario)
-prompt (nombreUsuario + "," + " ¿Quieres aprender a cuidar mejor de nuestro planeta?")
-alert ("Pues manos a la obra")
-console.log(nombreUsuario)
